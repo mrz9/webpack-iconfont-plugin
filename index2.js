2d7b4fa8b1708e8b0c6e2926e564fea945c05909
张小俊 <1711231134@qq.com>
2021-11-17 09:50:07 +0800

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,24 +1,31 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ora = require('ora');
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const ora = require('ora');
 
-module.exports = class MyPlugins {
+module.exports = class MrzPlugin {
   constructor(options) {
     this.options = options;
     this.name = this.constructor.name;
     this.loading = false;
   }
   apply(compiler) {
-    const { options } = compiler
+    const { options } = compiler;
+    // console.log(options.plugins)
     // console.log(options.plugins.HtmlWebpackPlugin)
     compiler.hooks.thisCompilation.tap(this.name, (compilation) => {
-      const hooks =  HtmlWebpackPlugin.getHooks(compilation);
-      Object.keys(hooks).forEach(hook => {
-        console.log('hook ', hook)
-        hooks[hook].tapAsync(this.name, (data, cb) => {
-          console.log('in ', hook, typeof cb)
-          typeof cb === 'function' && cb();
-        })
-      })
+      const HtmlWebpackPlugin = (compiler.options.plugins || []).find(
+        (plugin) => plugin.constructor.name === "HtmlWebpackPlugin"
+      );
+      if (HtmlWebpackPlugin) {
+          console.log("HtmlWebpackPlugin", HtmlWebpackPlugin);
+          const hooks = HtmlWebpackPlugin.constructor.getHooks(compilation);
+          Object.keys(hooks).forEach((hook) => {
+            console.log("hook ", hook);
+            hooks[hook].tapAsync(this.name, (data, cb) => {
+              console.log("in ", hook, typeof cb);
+              typeof cb === "function" && cb();
+            });
+          });
+      }
     });
     // compiler.hooks.thisCompilation.tap(this.name, (compilation) => {
     //   HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync(
@@ -50,6 +57,5 @@ module.exports = class MyPlugins {
     //   );
 
     // });
-
   }
 };
